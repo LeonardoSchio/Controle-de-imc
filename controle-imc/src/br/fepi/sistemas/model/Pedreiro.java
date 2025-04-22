3ea988cc74e8439bad9c0f55a46c46acde8f4d17
Leonardo Schio <leonardoschio92@gmail.com>
2025-04-22 19:53:38 -0300

    Criei um toString na classe Pedreiro e na classe Teste foi adicionado a escolha de uma profissão

--- controle-imc/src/br/fepi/sistemas/model/Pedreiro.java
+++ controle-imc/src/br/fepi/sistemas/model/Pedreiro.java
@@ -4,4 +4,9 @@ public class Pedreiro extends Pessoa {
     public Pedreiro(String nome, double peso, double altura) {
         super(nome, peso, altura);
     }
+
+    @Override
+    public String toString() {
+        return "*Pedreiro* \n" + super.toString();
+    }
 }
--- controle-imc/src/br/fepi/sistemas/principal/Teste.java
+++ controle-imc/src/br/fepi/sistemas/principal/Teste.java
@@ -1,5 +1,6 @@
 package br.fepi.sistemas.principal;
 
+import br.fepi.sistemas.model.Pedreiro;
 import br.fepi.sistemas.model.Pessoa;
 
 import java.util.ArrayList;
@@ -25,9 +26,20 @@ public class Teste {
             double altura = scanner.nextDouble();
             scanner.nextLine();
 
-            Pessoa p = new Pessoa(nome, peso, altura);
+            System.out.println("Escolha a sua profissão");
+            System.out.println("1 - Pessoa comum");
+            System.out.println("2 - Pedreiro");
+            int opcao = Integer.parseInt(scanner.nextLine());
+
+            Pessoa p;
+            if (opcao == 2) {
+                p = new Pedreiro(nome, peso, altura);
+            } else {
+                p = new Pessoa(nome, peso, altura);
+            }
+
             pessoas.add(p);
-            System.out.println("Pessoa cadastrada com sucesso!");
+            System.out.println("Pessoa cadastrada com sucesso! Profissão: " + (p instanceof Pedreiro ? "Pedreiro" : "Pessoa comum"));
         }
         scanner.close();
 
